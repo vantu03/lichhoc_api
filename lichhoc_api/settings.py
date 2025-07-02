3517eb8f1ce7d6d02c697daf0ec1e236faff658c
vantu03 <concaoheo203@gmail.com>
2025-07-02 22:43:25 +0700

    Fix CORS

--- lichhoc_api/settings.py
+++ lichhoc_api/settings.py
@@ -26,9 +26,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'lichhoc',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -109,3 +111,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "https://sv.pro.vn",  # domain frontend
+    "http://localhost:3000",  # nếu có frontend local
+]
+
