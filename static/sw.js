020c0e77b38dd0a27746c23773c338ca35d00f3d
vantu03 <concaoheo203@gmail.com>
2025-07-12 23:11:26 +0700

    commit

--- static/sw.js
+++ static/sw.js
@@ -1,7 +1,32 @@
-self.addEventListener("push", function(event) {
+self.addEventListener("push", function (event) {
     const data = event.data.json();
-    self.registration.showNotification(data.title, {
+
+    const options = {
         body: data.body,
-        icon: "/static/icon.png"
-    });
+        icon: "/static/icon.png",
+        data: {
+            url: "/" // URL mở khi click vào thông báo
+        }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener("notificationclick", function (event) {
+    event.notification.close();
+
+    event.waitUntil(
+        clients.matchAll({ type: "window", includeUncontrolled: true }).then(function (clientList) {
+            for (const client of clientList) {
+                if (client.url === event.notification.data.url && "focus" in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow(event.notification.data.url);
+            }
+        })
+    );
 });
