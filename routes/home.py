48fe75b387eb962e6e63f681e307a90a296de169
vantu03 <concaoheo203@gmail.com>
2025-07-24 19:02:44 +0700

    commit

--- routes/home.py
+++ routes/home.py
@@ -1,13 +1,13 @@
 # routes/home.py
 from flask import Blueprint, render_template
-from models import FirebaseCredential, Token
+from models import FirebaseCredential, FcmToken
 
 home_bp = Blueprint('home', __name__)
 
 @home_bp.route('/')
 def home():
     firebase_credentials = FirebaseCredential.query.all()
-    tokens = [t.token for t in Token.query.all()]
+    tokens = [t.token for t in FcmToken.query.all()]
     return render_template('home.html', tokens=tokens, firebase_credentials=firebase_credentials)
 
 @home_bp.route('/ping')
