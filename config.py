ebc0b3e33d4ef6f01238c0afb11d73ead065e8c0
vantu03 <concaoheo203@gmail.com>
2025-07-24 16:37:05 +0700

    commit

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# config.py
 import os
+from dotenv import load_dotenv
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://svprovn_vantu:DvAHy7j6Sfk%40GWw@sv.pro.vn:3306/svprovn_svpro'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     SQLALCHEMY_ENGINE_OPTIONS = {
         "pool_pre_ping": True,
         "pool_recycle": 280
